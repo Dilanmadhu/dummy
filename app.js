f2b276b4b991b21608882d1d88a2d84538912691
Dilan Madhusankha <dilanmadusanka5555@gmail.com>
2024-08-05 21:54:26 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const app = express();
+
+app.get("/package/fetch/:id", (req, res, next)=>{
+    const packageId = req.params.id;
+    const responseObject = {
+        "package": {
+            "package_id" : packageId,
+            "tag_id": 54215761,
+            "type": "furniture",
+            "package_condition": "new",
+            "destination": "colombo",
+            "price": 13200,
+            "tracking_device_id": 1
+        },
+        "sender": {
+            "email": "jhon.co@m.am",
+            "first_name": "jhon",
+            "last_name": "cina",
+            "mobile_number": 771885421
+        },
+        "receiver": {
+            "email": "brayan@g.com",
+            "first_name": "brayan",
+            "last_name": "adams",
+            "mobile_number": 771234567
+        }
+    };
+
+    res.json(responseObject);
+
+})
+
+app.listen(3000, ()=>{
+    console.log("server is running at port 3000")
+})
